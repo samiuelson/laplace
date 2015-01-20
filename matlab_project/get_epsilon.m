fc83eb5818921569c0cc117c4cc27c72c325d887
Sami <samiuelson@gmail.com>
2015-01-20 13:05:11 +0100

    needed functions files

---
+++ matlab_project/get_epsilon.m
@@ -0,0 +1,9 @@
+%%
+% Funkcja zwracajaca epsilon dla danych pozycji siatki j,k
+%%
+function eps = get_epsilon(j,k)
+eps=-1;
+
+
+
+return eps;
---
+++ matlab_project/get_v.m
@@ -0,0 +1,11 @@
+%%
+% Funkcja zwracajaca epsilon dla danych pozycji siatki j,k
+%%
+function v = get_v(j,k)
+v=0;
+
+
+
+
+
+return v;
